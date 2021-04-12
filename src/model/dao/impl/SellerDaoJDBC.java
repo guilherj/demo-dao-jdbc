f10561a0f1e1ac3190412c818b6cd6cc03b25b1f
guilherj <contato.guilhermenascimento@hotmail.com>
2021-04-12 13:05:03 -0300

    Reusing instantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -22,19 +22,19 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
@@ -45,27 +45,17 @@ public class SellerDaoJDBC implements SellerDao {
 		ResultSet rs = null;
 
 		try {
+			// COMANDO SQL PARA BUSCA DE SELLER POR ID
+			st = conn.prepareStatement("SELECT seller.*,department.Name as DepName FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ?");
 
-			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
-							+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ?");
-
-			st.setInt(1, id);
+			st.setInt(1, id); // <-- SETANDO O ID DA SENTENÇA SQL COMO O ID PASSADO POR ARGUMENTO NO MÉTODO
 			rs = st.executeQuery();
 
 			if (rs.next()) {
 
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
-
-				Seller obj = new Seller();
-				obj.setId(rs.getInt("Id"));
-				obj.setName(rs.getString("Name"));
-				obj.setEmail(rs.getString("Email"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setBirthDate(rs.getDate("BirthDate"));
-				obj.setDepartment(dep);
+				Department dep = instantiateDepartment(rs); // USANDO O MÉTODO AUXILIAR DECLARADO ABAIXO
+				Seller obj = instantiateSeller(rs, dep); // USANDO O MÉTODO AUXILIAR DECLARADO ABAIXO 
 				return obj;
 			}
 			return null;
@@ -76,13 +66,49 @@ public class SellerDaoJDBC implements SellerDao {
 		} finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
+			/*
+			 * NÃO É PRECISO FECHAR A CONEXÃO COM O BANCO, POIS ESSE DAO PODE SER USADO
+			 * PARA OUTRAS OPERAÇÕES, ENTÃO PODE DEIXAR PARA FECHAR A CONEXÃO NA CLASSE PROGRAM.
+			 */
 		}
 
 	}
 
+	/*
+	 * OS MÉTODOS ABAIXO (instantiateSeller e instantiateDepartment) SERVEM COMO
+	 * AUXILIARES PARA MELHORAR O CÓDIGO DO MÉTODO FindById, ASSIM ELE NÃO FICA
+	 * MUITO GRANDE E A CLASSE FICA MAIS ORGANIZADA.
+	 * 
+	 * AS EXCESSÕES QUE PODEM DAR NESSES MÉTODOS ESTÃO SENDO PROPAGADAS PARA QUE SEJAM TRATADAS PELO CATCH
+	 * QUANDO FOREM CHAMADOS NO MÉTODO FindById.
+	 * 
+	 * O CÓDIGO DESSES MÉTODOS ESTÃO INSTANCIANDO OS OBJETOS COM OS DADOS RECEBIDOS PELO
+	 * RESULTSET "rs" É NECESSÁRIO FAZER ESSA OPERAÇÃO POIS ESTAMOS PROGRAMANDO NUMA
+	 * LINGUAGEM ORIENTADA A OBJETO ENTÃO DEVE-SE INSTANCIAR OS OBJETOS COM SUAS
+	 * ASSOCIAÇÕES NA MEMÓRIA.
+	 */
+
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);
+		return obj;
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
+	}
+
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
