267a37c7575710eafc92c52780057f458b18c6f5
guilherj <contato.guilhermenascimento@hotmail.com>
2021-04-17 13:07:08 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,27 +14,33 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println("==== TEST 1: seller findById");		
+		System.out.println("==== TEST 1: seller findById ====");		
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-		System.out.println("\n==== TEST 2: seller findByDepartmentId");
+		System.out.println("\n==== TEST 2: seller findByDepartmentId ====");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n==== TEST 3: seller findAll");
+		System.out.println("\n==== TEST 3: seller findAll ====");
 		list = sellerDao.findAll();
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n==== TEST 4: seller insert");
+		System.out.println("\n==== TEST 4: seller insert ====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted!, New Id = " + newSeller.getId());
+		
+		System.out.println("\n==== TEST 5: seller upddate ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Updated!");
 		
 	}
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -27,39 +27,39 @@ public class SellerDaoJDBC implements SellerDao {
 	@Override
 	public void insert(Seller obj) {
 		PreparedStatement st = null;
-		
+
 		try {
-			st = conn.prepareStatement("INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) "
-										+ "VALUES (?, ?, ?, ?, ?)" , Statement.RETURN_GENERATED_KEYS);
-			
+			st = conn.prepareStatement(
+					"INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) " + "VALUES (?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
 			st.setDouble(4, obj.getBaseSalary());
 			st.setInt(5, obj.getDepartment().getId());
-			
+
 			int rowsAffected = st.executeUpdate();
-			
-			if(rowsAffected > 0) { 
-				/* 
-				 * USADO if POR QUE A INSERÇÃO É DE APENAS UM VALOR, CASO TIVESSE INSERINDO MAIS DE UM VALOR
-				 * TERIA QUE USAR O WHILE
+
+			if (rowsAffected > 0) {
+				/*
+				 * USADO if POR QUE A INSERÇÃO É DE APENAS UM VALOR, CASO TIVESSE INSERINDO MAIS
+				 * DE UM VALOR TERIA QUE USAR O WHILE
 				 */
 				ResultSet rs = st.getGeneratedKeys();
-				if(rs.next()) {
+				if (rs.next()) {
 					int id = rs.getInt(1);
 					obj.setId(id);
 				}
 				DB.closeResultSet(rs);
-				
+
 			} else {
 				throw new DbException("Unexpected error! No rows Affected!");
 			}
-			
-			
+
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-			
+
 		} finally {
 			DB.closeStatement(st);
 		}
@@ -68,6 +68,27 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void update(Seller obj) {
+		PreparedStatement st = null;
+
+		try {
+			st = conn.prepareStatement("UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? WHERE Id = ?");
+
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+
+			st.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+
+		} finally {
+			DB.closeStatement(st);
+		}
 
 	}
 
