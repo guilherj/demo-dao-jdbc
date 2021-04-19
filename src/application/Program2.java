2beeeeac551d84b7a33a9aaebee5b02be6b02a81
guilherj <contato.guilhermenascimento@hotmail.com>
2021-04-19 11:30:16 -0300

    Created Department Dao JDBC, Department.insert, Department.update and tested Department.insert

---
+++ src/application/Program2.java
@@ -0,0 +1,24 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		/*
+		System.out.println("==== TEST 1: seller findById ====");
+		Department department = new Department(null, "Teste");
+		departmentDao.insert(department);
+		System.out.println("Inserted! Department Id:" + department.getId());
+		*/
+		
+		
+
+	}
+
+}
